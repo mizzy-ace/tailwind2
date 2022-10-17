401fe79b8f3dcad69a199f9251f9e6d21af98023
= <johndeprogramer@gmail.com>
2022-10-17 23:51:09 +0000

    h

--- v1/src/App.js
+++ v1/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center"> fkf k</h1>
+    <div className="App bg-[#00df9a]">
+      <h1 className="text-center">fkf  k</h1>
     </div>
   );
 }
