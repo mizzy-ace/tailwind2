9aaa4eeb40f17697f399957fd827c1cbfa3777ac
= <johndeprogramer@gmail.com>
2022-10-17 23:59:20 +0000

    gg

--- v1/src/App.js
+++ v1/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App bg-[#00df9a]">
-      <h1 className="text-center">fkf  k</h1>
+      <h1 className="text-center">fkf k</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ v1/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='text-4xl text-red-500'>NavBar</div>
+  )
+}
+
+export default NavBar
